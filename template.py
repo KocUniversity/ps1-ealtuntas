6e6c3e8f0ac481a2287269a62b59f4d1a8548fad
ealtuntas <ealtuntas19@ku.edu.tr>
2021-03-17 00:18:52 +0000

    :(((

--- template.py
+++ template.py
@@ -1,5 +1,34 @@
-n, B = list(map(int, input().strip().split()))
+# n, B = list(map(int, input().strip().split()))
+n = int(input('Enter the number of elements in the sequence: '))
+B = int(input('Enter the lower bound: '))
+
+
 T = 0
+
+i = 1
+remainder = i % 2
+if remainder == 0:
+  p_i = 3 ** i + 1
+
+  i += 2
+
+print(T)
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
 
 # your code here
 
@@ -7,3 +36,21 @@ T = 0
 # please do not modify the input and print statements
 # and make sure that your code does not have any other print statements
 print(T)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n, B = list(map(int, input().strip().split()))
